e15f8b9c3f2dccc2b8dbf7e300b48af8f0946337
viktorx64 <viktor.sundqvist2@gmail.com>
2021-01-14 12:43:41 +0100

    made interface

---
+++ bilar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        
+    }
+
+    interface ICar
+    {
+        void Distance();
+        void Service();
+        void Cost();
+        void Broken();
+    }
+
+    class Car : ICar
+    {
+        public void Distance ()
+        {
+            Console.WriteLine("car has ran 2000Mil");
+        }
+        public void Service()
+        {
+            Console.WriteLine("car was last serviced 2020-05-14");
+        }
+        public void Cost()
+        {
+            Console.WriteLine("car cost 15 000 sek");
+        }
+        public void Broken()
+        {
+            Console.WriteLine("car has a scratch on the rear-left door");
+        }
+    }
+}
